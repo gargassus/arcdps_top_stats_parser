ccb326724806a4427c738332c03b555f1804d876
Drevarr <drevarr@gmail.com>
2024-10-15 11:24:57 -0400

    add team code 698: 'Red'

--- TW5_parse_top_stats_tools.py
+++ TW5_parse_top_stats_tools.py
@@ -3643,7 +3643,7 @@ def get_stats_from_fight_json(fight_json, config, log):
 	squad_damage_output[fight_name] = {}
 
 	current_Tag = ''
-	teamID = {705: 'Red', 706: 'Red', 882: 'Red', 2520: 'Red', 2739: 'Green', 2741: 'Green', 2752: 'Green', 2763: 'Green', 432: 'Blue', 1277: 'Blue'}
+	teamID = {698: 'Red', 705: 'Red', 706: 'Red', 882: 'Red', 2520: 'Red', 2739: 'Green', 2741: 'Green', 2752: 'Green', 2763: 'Green', 432: 'Blue', 1277: 'Blue'}
 
 	#creat dictionary of skill_ids and skill_names
 	skills = fight_json['skillMap']
