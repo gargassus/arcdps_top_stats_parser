f4c4e35a6c052f33b3aad2eb30c876638c2c52e8
darkharasho <144265798+darkharasho@users.noreply.github.com>
2025-03-14 15:53:23 -0700

    Improve the exist check to work with spaces

--- TW5_parsing_arc_top_stats.bat
+++ TW5_parsing_arc_top_stats.bat
@@ -1,22 +1,22 @@
 @echo off
-setlocal
+setlocal EnableDelayedExpansion
 
 rem Check which executable exists
-if exist "%2\GuildWars2EliteInsights.exe" (
-    set "EI_EXEC=%2\GuildWars2EliteInsights.exe"
-) else if exist "%2\GuildWars2EliteInsights-CLI.exe" (
-    set "EI_EXEC=%2\GuildWars2EliteInsights-CLI.exe"
+if exist "%~2\GuildWars2EliteInsights.exe" (
+    set "EI_EXEC=%~2\GuildWars2EliteInsights.exe"
+) else if exist "%~2\GuildWars2EliteInsights-CLI.exe" (
+    set "EI_EXEC=%~2\GuildWars2EliteInsights-CLI.exe"
 ) else (
-    echo No valid Guild Wars 2 Elite Insights executable found.
+    echo No valid Guild Wars 2 Elite Insights executable found in %2
     exit /b 1
 )
 
 rem Process .zevtc files
-for %%i in (%1\*.zevtc) do (
-    "%EI_EXEC%" -c %3\EI_config\EI_detailed_json_combat_replay.conf "%%i"
+for %%i in ("%~1\*.zevtc") do (
+    "!EI_EXEC!" -c "%~3\EI_config\EI_detailed_json_combat_replay.conf" "%%i"
 )
 
 rem Run the Python script
-python %3/TW5_parse_top_stats_detailed.py %1
+python "%~3/TW5_parse_top_stats_detailed.py" "%~1"
 
 endlocal
